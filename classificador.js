575857b11d2d47fd5c2798940d8b07bcd3da64c5
Marcio Viana <vianalmarcio@gmail.com>
2023-11-16 00:27:58 -0300

    desenvolver desafio da DIO

---
+++ classificador.js
@@ -0,0 +1,23 @@
+let nome = "Kratos"
+let nivel= " "
+
+nivel = 8500
+
+if (nivel <= 1000) {
+    console.log ("O herói de nome " + nome + " está no nível ferro")}
+    else if (nivel >= 1001 & nivel <= 2000) {
+        console.log ("o herói de nome "+ nome + " está no nível bronze")}
+        else if (nivel >= 2001 & nivel <= 5000){
+            console.log ("o herói de nome "+ nome + " está no nível prata")}
+            else if (nivel >= 5001 & nivel <= 7000){
+                console.log ("o herói de nome " + nome + " está no nível ouro")}
+                    else if (nivel >= 7001 & nivel <= 8000){
+                        console.log ("o herói de nome " + nome + " está no nivel platina")}
+                            else if (nivel >= 8001 & nivel <= 9000){
+                                console.log ("o herói de nome " + nome + " está no nivel ascendente")}
+                                    else if (nivel >= 9001 & nivel <= 10000){
+                                        console.log ("o herói de nome " + nome + " está no nivel imortal")}
+                                    else {
+                                        console.log ("o herói de nome " + nome + " está no nivel radiante")
+                                    }        
+                                
